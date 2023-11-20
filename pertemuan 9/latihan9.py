50e8d56acf792595eff6e28f649dd9e661c02b4f
annisasaidah06 <148035766+annisasaidah06@users.noreply.github.com>
2023-11-20 19:53:03 +0700

    Add files via upload

---
+++ pertemuan 9/latihan9.py
@@ -0,0 +1,23 @@
+# Buat list dengan 5 elemen list_a = [10, 20, 30, 40, 50]
+print("Elemen ke-3:", list a[2])
+
+print("Elemen ke-2 sampai ke-4:", [1:4])
+print("Elemen terakhir:",[-1])
+
+list [3] = 45  ("Setelah mengubah elemen ke-4:", list_a)
+
+
+list_a[3:] = [50, 60, 70] ("Setelah mengubah elemen ke-4 sampai terakhir:", )
+
+
+list_b = l_a[:2] ("List B:", list_b)
+
+
+list_b.append("Hello") ("List B setelah tambah string:", list_b)
+
+
+list_b.extend([1, 2, 3]) ("List B setelah tambah 3 nilai:", list_b)
+
+
+list_c = list a + list_b ("List setelah menggabungkan list A dan B:", list_c)
+
